95afbace305c195fa470f4420313f2259c108760
Ricardo Genaizir <rgenaizir@mobydigital.com>
2021-05-18 15:03:12 -0300

    first commit

---
+++ src/Main2.java
@@ -0,0 +1,11 @@
+
+public class Main2 {
+
+public static void main(String[] args) {
+	String numeros = "";
+	Integer num  = 23;
+	String num2 = numeros.concat(" " + num);
+	System.out.println(num2);
+}
+
+}
